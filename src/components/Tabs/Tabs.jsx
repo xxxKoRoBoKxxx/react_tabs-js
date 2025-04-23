fedc63168f8aaa81c725072123ef6c4ac13184a2
Andrii Filipenko <andreyfil1337@gmail.com>
2025-04-23 19:41:16 +0300

    progress

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,29 @@
-export const Tabs = () => {};
+import classNames from 'classnames/bind';
+
+export const Tabs = ({ tabs, activeTabId, setActiveTabId }) => {
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => {
+            return (
+              <li
+                className={classNames({ 'is-active': activeTabId === tab.id })}
+                data-cy="Tab"
+                onClick={}
+              >
+                <a href={`#${tab.id}`} data-cy="TabLink">
+                  {tab.title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        Some text 1
+      </div>
+    </div>
+  );
+};
